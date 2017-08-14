e94119c088e75d549e64d893dc736d4734727dc4
Roman <nevermind1993k@gmail.com>
2017-08-14 18:04:27 +0300

    RadioButton module added

--- RadioButtons/src/Form.java
+++ RadioButtons/src/Form.java
@@ -3,10 +3,10 @@ import javax.swing.*;
 public class Form {
     public static void main(String[] args) {
         FormFrame frame = new FormFrame();
-        frame.setSize(200, 350);
+        frame.setSize(200, 400);
         frame.setTitle("Form v1.0");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-//        frame.pack();
+        //frame.pack();
     }
 }
